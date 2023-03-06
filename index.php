985b13aac706629778dea9abfaea9c459638ba8c
Aaron24115 <9018817@student.zadkine.nl>
2023-03-06 14:00:17 +0100

    Inlogpagina en mainpagina aangemaakt

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+$dsn = "mysql:host=localhost;dbname=aanmelden";
+$user = "root";
+$pass = "";
+$conn =  new PDO($dsn, $user, $pass);
+
+$sql = "SELECT*FROM aanmelding";
+
+$stmt = $conn->query($sql);
+
+while ( $row = $stmt->fetch(PDO::FETCH_OBJ)) {
+    echo "<br>" . $row->Achternaam;
+    echo "<pre>".print_r($row, true)."</pre>";
+}
